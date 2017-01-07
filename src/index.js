e7fea39d7fca8f76f9958ba7ce8e517a8d33cb79
Salvatore Laisa <moebiusmania@gmail.com>
2017-01-07 22:29:24 +0100

    first basic component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// Code here
+// Import libraries ( ES6 import )
+import React from 'react';
+import { render } from 'react-dom';
+
+// Javascript class
+class Hello extends React.Component {
+  // Functions inside classes are called "methods"
+  render(){
+    // JSX: HTML inside Javascript
+    return (
+      <div className="alert alert-info">
+        Hello <strong>React</strong>
+      </div>
+    )
+  }
+}
+
+// Render the application inside a DOM node
+render(<Hello />, document.getElementById('app'));
